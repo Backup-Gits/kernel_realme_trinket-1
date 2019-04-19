efbc10ffc4a79870bc967e6fecc8e0c5d160ae35
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-18 22:07:57 -0700

    devfreq_boost: Introduce devfreq boost driver
    This driver boosts enumerated devfreq devices upon input, and allows for
    boosting specific devfreq devices on other custom events. The boost
    frequencies for this driver should be set so that frame drops are
    near-zero at the boosted frequencies and power consumption is minimized
    at said frequencies. The goal of this driver is to provide an interface
    to achieve optimal device performance by requesting boosts on key
    events, such as when a frame is ready to rendered to the display.
    
    Currently, support is only present for boosting the cpubw devfreq
    device, but the driver is structured in a way that makes it easy to add
    support for new boostable devfreq devices in the future.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [yaro: adjusted for 4.14 (soc:qcom,cpubw->soc:qcom,cpu-llcc-ddr-bw]
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- include/linux/devfreq.h
+++ include/linux/devfreq.h
@@ -156,6 +156,8 @@ struct devfreq {
 
 	unsigned long min_freq;
 	unsigned long max_freq;
+	bool is_boost_device;
+	bool max_boost;
 	bool stop_polling;
 
 	/* information for device frequency transition */
@@ -288,6 +290,9 @@ struct devfreq_passive_data {
 };
 #endif
 
+/* Caution: devfreq->lock must be locked before calling update_devfreq */
+extern int update_devfreq(struct devfreq *devfreq);
+
 #else /* !CONFIG_PM_DEVFREQ */
 static inline struct devfreq *devfreq_add_device(struct device *dev,
 					  struct devfreq_dev_profile *profile,
@@ -401,11 +406,11 @@ static inline int devfreq_set_limit(struct devfreq *df, unsigned long min, unsig
         return -EINVAL;
 }
 
-static inline int devfreq_get_limit(struct devfreq *df, unsigned long *min, unsigned long *max)
-{
-        return -EINVAL;
-}
 #endif /* CONFIG_PRODUCT_REALME_TRINKET */
+static inline int update_devfreq(struct devfreq *devfreq)
+{
+	return -EINVAL;
+}
 #endif /* CONFIG_PM_DEVFREQ */
 
 #endif /* __LINUX_DEVFREQ_H__ */
