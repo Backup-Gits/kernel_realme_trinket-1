b04e836750214b0a89eb83f2921d36f8ea3306f6
XNXXGANG <fadlidodo2004@gmail.com>
2021-06-15 02:50:05 +0000

    Revert "sched/tune: Disable boost & prefer_idle if battery saver is active"
    This reverts commit 7b3f0d0db44c7b32726e39157bf7437939e9bec2.

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -5,7 +5,6 @@
 #include <linux/printk.h>
 #include <linux/rcupdate.h>
 #include <linux/slab.h>
-#include <linux/battery_saver.h>
 
 #include <trace/events/sched.h>
 #ifdef CONFIG_PRODUCT_REALME_TRINKET
@@ -695,7 +694,7 @@ int schedtune_task_boost(struct task_struct *p)
 	struct boost_groups *bg;
 #endif
 
-	if (unlikely(!schedtune_initialized) || is_battery_saver_on())
+	if (unlikely(!schedtune_initialized))
 		return 0;
 
 	/* Get task boost value */
@@ -720,7 +719,7 @@ int schedtune_prefer_idle(struct task_struct *p)
 	struct schedtune *st;
 	int prefer_idle;
 
-	if (unlikely(!schedtune_initialized) || is_battery_saver_on())
+	if (unlikely(!schedtune_initialized))
 		return 0;
 
 	/* Get prefer_idle value */
@@ -736,9 +735,6 @@ static u64
 prefer_idle_read(struct cgroup_subsys_state *css, struct cftype *cft)
 {
 	struct schedtune *st = css_st(css);
-
-	if (is_battery_saver_on())
-		return 0;
 
 	return st->prefer_idle;
 }
@@ -757,9 +753,6 @@ static s64
 boost_read(struct cgroup_subsys_state *css, struct cftype *cft)
 {
 	struct schedtune *st = css_st(css);
-
-	if (is_battery_saver_on())
-		return 0;
 
 	return st->boost;
 }
